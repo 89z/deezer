ed568c1a67633a61e835553c7c6efcdcaa421842
Steven Penny <srpen6@gmail.com>
2021-03-04 09:02:45 -0600

    deleted:    aes-ecb-cipher.go

--- Struct.go
+++ Struct.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+   "bytes"
+   "crypto/aes"
+   "crypto/cipher"
    "encoding/json"
+   "errors"
    "flag"
    "fmt"
+   "golang.org/x/crypto/blowfish"
    "os"
 )
 
 var cfg = new(Config)
+
+func BFDecrypt(buf []byte, bfKey string) ([]byte, error) {
+   decrypter, err := blowfish.NewCipher([]byte(bfKey)) // 8bytes
+   if err != nil {
+      return nil, err
+   }
+   IV := []byte{0, 1, 2, 3, 4, 5, 6, 7} //8 bytes
+   if len(buf)%blowfish.BlockSize != 0 {
+      return nil, errors.New("The Buf is not a multiple of 8")
+   }
+   cbcDecrypter := cipher.NewCBCDecrypter(decrypter, IV)
+   cbcDecrypter.CryptBlocks(buf, buf)
+   return buf, nil
+}
 
 func ErrorUsage() {
    fmt.Println(`Guide: go-decrypt-deezer [--debug --id --usertoken`)
@@ -15,6 +34,12 @@ func ErrorUsage() {
    fmt.Println(`Example: go-decrypt-deezer --id 3135556 --usertoken UserToken_here`)
    flag.PrintDefaults()
    os.Exit(1)
+}
+
+func Pad(src []byte) []byte {
+   padding := aes.BlockSize - len(src)%aes.BlockSize
+   padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+   return append(src, padtext...)
 }
 
 func init() {
@@ -67,4 +92,63 @@ type TrackData struct {
    MediaVersion json.Number `json:"MEDIA_VERSION"`
    SngTitle     string      `json:"SNG_TITLE"`
    ArtName      string      `json:"ART_NAME"`
+}
+
+type ecb struct {
+   b         cipher.Block
+   blockSize int
+}
+
+func newECB(b cipher.Block) *ecb {
+   return &ecb{
+      b, b.BlockSize(),
+   }
+}
+
+type ecbDecrypter ecb
+
+func NewECBDecrypter(b cipher.Block) cipher.BlockMode {
+   return (*ecbDecrypter)(newECB(b))
+}
+
+func (x *ecbDecrypter) BlockSize() int {
+   return x.blockSize
+}
+
+func (x *ecbDecrypter) CryptBlocks(dst, src []byte) {
+   if len(src)%x.blockSize != 0 {
+      panic("crypto/cipher: input not full blocks")
+   }
+   if len(dst) < len(src) {
+      panic("crypto/cipher: output smaller than input")
+   }
+   for len(src) > 0 {
+      x.b.Decrypt(dst, src[:x.blockSize])
+      src = src[x.blockSize:]
+      dst = dst[x.blockSize:]
+   }
+}
+
+type ecbEncrypter ecb
+
+func NewECBEncrypter(b cipher.Block) cipher.BlockMode {
+   return (*ecbEncrypter)(newECB(b))
+}
+
+func (x *ecbEncrypter) BlockSize() int {
+   return x.blockSize
+}
+
+func (x *ecbEncrypter) CryptBlocks(dst, src []byte) {
+   if len(src)%x.blockSize != 0 {
+      panic("crypto/cipher: input not full blocks")
+   }
+   if len(dst) < len(src) {
+      panic("crypto/cipher: output smaller than input")
+   }
+   for len(src) > 0 {
+      x.b.Encrypt(dst, src[:x.blockSize])
+      src = src[x.blockSize:]
+      dst = dst[x.blockSize:]
+   }
 }
