1d9a9740e2efc33178ceb3905b2c494dd52087d0
Steven Penny <srpen6@gmail.com>
2021-03-04 11:30:52 -0600

    +   Secret := "g4el58wc0zvf9na1"

--- utils.go
+++ utils.go
@@ -58,116 +58,110 @@ func addQs(req *http.Request, args ...string) *http.Request {
 
 // GetBlowFishKey get the BlowFishkey for decryption
 func GetBlowFishKey(id string) string {
-	Secret := "g4el58wc0zvf9na1"
-	md5Sum := md5.Sum([]byte(id))
-	idM5 := fmt.Sprintf("%x", md5Sum)
-
-	var BFKey string
-	for i := 0; i < 16; i++ {
-		BFKey += fmt.Sprintf("%s", string(idM5[i]^idM5[i+16]^Secret[i]))
-	}
-
-	return BFKey
+   Secret := "g4el58wc0zvf9na1"
+   md5Sum := md5.Sum([]byte(id))
+   idM5 := fmt.Sprintf("%x", md5Sum)
+   var BFKey string
+   for i := 0; i < 16; i++ {
+      BFKey += fmt.Sprintf("%s", string(idM5[i]^idM5[i+16]^Secret[i]))
+   }
+   return BFKey
 }
 
 // GetToken get the login token
 func GetToken(client *http.Client) (string, *OnError) {
-	Deez := &DeezStruct{}
-	args := []string{"null", "deezer.getUserData"}
-	reqs, err := newRequest(APIUrl, "GET", nil)
-	if err != nil {
-		return "", &OnError{err, "Error during GetToken GET request"}
-	}
-	reqs = addQs(reqs, args...)
-	resp, err := client.Do(reqs)
-	if err != nil {
-		return "", &OnError{err, "Error during GetToken response"}
-	}
-
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	err = json.Unmarshal(body, &Deez)
-	if err != nil {
-		return "", &OnError{err, "Error During Unmarshal"}
-	}
-	APIToken := Deez.Results.DeezToken
-	return APIToken, nil
+   Deez := &DeezStruct{}
+   args := []string{"null", "deezer.getUserData"}
+   reqs, err := newRequest(APIUrl, "GET", nil)
+   if err != nil {
+      return "", &OnError{err, "Error during GetToken GET request"}
+   }
+   reqs = addQs(reqs, args...)
+   resp, err := client.Do(reqs)
+   if err != nil {
+      return "", &OnError{err, "Error during GetToken response"}
+   }
+   defer resp.Body.Close()
+   body, _ := ioutil.ReadAll(resp.Body)
+   err = json.Unmarshal(body, &Deez)
+   if err != nil {
+      return "", &OnError{err, "Error During Unmarshal"}
+   }
+   APIToken := Deez.Results.DeezToken
+   return APIToken, nil
 }
 
 // DecryptDownload Get the encrypted download link
 func DecryptDownload(md5Origin, songID, format, mediaVersion string) (string, error) {
-	urlPart := md5Origin + "¤" + format + "¤" + songID + "¤" + mediaVersion
-	data := bytes.Replace([]byte(urlPart), []byte("¤"), []byte{164}, -1)
-	md5SumVal := fmt.Sprintf("%x", md5.Sum(data))
-	urlPart = md5SumVal + "¤" + urlPart + "¤"
-
-	// Encrypt urlPart in hex format
-	key := []byte("jo6aey6haid2Teih")
-	plaintext := Pad(bytes.Replace([]byte(urlPart), []byte("¤"), []byte{164}, -1))
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
-	}
-	encryptText := make([]byte, len(plaintext))
-	mode := NewECBEncrypter(block) // return ECB encryptor
-	mode.CryptBlocks(encryptText, plaintext)
-	return "https://e-cdns-proxy-" + md5Origin[:1] + ".dzcdn.net/mobile/1/" + fmt.Sprintf("%x", encryptText),
-		nil
+   urlPart := md5Origin + "¤" + format + "¤" + songID + "¤" + mediaVersion
+   data := bytes.Replace([]byte(urlPart), []byte("¤"), []byte{164}, -1)
+   md5SumVal := fmt.Sprintf("%x", md5.Sum(data))
+   urlPart = md5SumVal + "¤" + urlPart + "¤"
+   key := []byte("jo6aey6haid2Teih")
+   plaintext := Pad(bytes.Replace([]byte(urlPart), []byte("¤"), []byte{164}, -1))
+   block, err := aes.NewCipher(key)
+   if err != nil {
+      return "", err
+   }
+   encryptText := make([]byte, len(plaintext))
+   mode := NewECBEncrypter(block) // return ECB encryptor
+   mode.CryptBlocks(encryptText, plaintext)
+   return fmt.Sprintf(
+      "https://e-cdns-proxy-%v.dzcdn.net/mobile/1/%x",
+      md5Origin[:1],
+      encryptText,
+   ), nil
 }
 
 // DecryptMedia decrypts the encrypted media that is returned by Deezer's server
 func DecryptMedia(stream io.Reader, id, FName string, streamLen int64) error {
-	// fmt.Println("Gopher is decrypting the media file")
-	var wg sync.WaitGroup
-	chunkSize := 2048
-	bfKey := GetBlowFishKey(id)
-	errc := make(chan error)
-	var err error
-	var destBuffer bytes.Buffer // final Product
-	for position, i := 0, 0; position < int(streamLen); position, i = position+chunkSize, i+1 {
-		func(i, position int, streamLen int64, stream io.Reader) {
-			var chunkString []byte
-			// check if stream is of 2048
-			if (int(streamLen) - position) >= 2048 {
-				chunkSize = 2048
-			} else {
-				chunkSize = int(streamLen) - position
-			}
-			buf := make([]byte, chunkSize) // The "chunk" of data
-			if _, err = io.ReadFull(stream, buf); err != nil {
-				errc <- errors.Wrapf(err, "error at loop %v", i)
-			}
-			if i%3 > 0 || chunkSize < 2048 {
-				chunkString = buf
-			} else { //Decrypt and then write to destBuffer
-				chunkString, err = BFDecrypt(buf, bfKey)
-				if err != nil {
-					errc <- errors.Wrapf(err, "error at loop %v", i)
-				}
-			}
-			if _, err := destBuffer.Write(chunkString); err != nil {
-				errc <- errors.Wrapf(err, "error at loop %v", i)
-			}
-		}(i, position, streamLen, stream)
-	}
-	for {
-		select {
-		case err = <-errc:
-			return err
-		default:
-			wg.Wait()
-			NameWithoutSlash := strings.ReplaceAll(FName, "/", " ")
-                        out, err := os.Create(NameWithoutSlash)
-			if err != nil {
-				return err
-			}
-			_, err = destBuffer.WriteTo(out)
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-	}
-
+   var wg sync.WaitGroup
+   chunkSize := 2048
+   bfKey := GetBlowFishKey(id)
+   errc := make(chan error)
+   var err error
+   var destBuffer bytes.Buffer // final Product
+   for position, i := 0, 0; position < int(streamLen); position, i = position+chunkSize, i+1 {
+      func(i, position int, streamLen int64, stream io.Reader) {
+         var chunkString []byte
+         if (int(streamLen) - position) >= 2048 {
+            chunkSize = 2048
+         } else {
+            chunkSize = int(streamLen) - position
+         }
+         buf := make([]byte, chunkSize) // The "chunk" of data
+         if _, err = io.ReadFull(stream, buf); err != nil {
+            errc <- errors.Wrapf(err, "error at loop %v", i)
+         }
+         if i%3 > 0 || chunkSize < 2048 {
+            chunkString = buf
+         } else { //Decrypt and then write to destBuffer
+            chunkString, err = BFDecrypt(buf, bfKey)
+            if err != nil {
+               errc <- errors.Wrapf(err, "error at loop %v", i)
+            }
+         }
+         if _, err := destBuffer.Write(chunkString); err != nil {
+            errc <- errors.Wrapf(err, "error at loop %v", i)
+         }
+      }(i, position, streamLen, stream)
+   }
+   for {
+      select {
+      case err = <-errc:
+         return err
+      default:
+         wg.Wait()
+         NameWithoutSlash := strings.ReplaceAll(FName, "/", " ")
+         out, err := os.Create(NameWithoutSlash)
+         if err != nil {
+            return err
+         }
+         _, err = destBuffer.WriteTo(out)
+         if err != nil {
+            return err
+         }
+         return nil
+      }
+   }
 }
