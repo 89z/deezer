4b5cd953a80a968e06b3b52d679b239a90f0e624
Steven Penny <srpen6@gmail.com>
2021-03-04 10:25:07 -0600

    renamed:    Utils.go -> utils.go

--- main.go
+++ main.go
@@ -1,87 +1,83 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/http/cookiejar"
-	"net/url"
-	"strconv"
-	"time"
+   "encoding/json"
+   "fmt"
+   "io/ioutil"
+   "log"
+   "net/http"
+   "net/http/cookiejar"
+   "net/url"
+   "strconv"
+   "time"
 )
 
 const (
-	// APIUrl is the deezer API
-	APIUrl = "http://www.deezer.com/ajax/gw-light.php"
-	// LoginURL is the API for deezer login
-	LoginURL = "https://www.deezer.com/ajax/action.php"
-	// deezer domain for cookie check
-	Domain = "https://www.deezer.com"
+   // APIUrl is the deezer API
+   APIUrl = "http://www.deezer.com/ajax/gw-light.php"
+   // LoginURL is the API for deezer login
+   LoginURL = "https://www.deezer.com/ajax/action.php"
+   // deezer domain for cookie check
+   Domain = "https://www.deezer.com"
 )
 
 func main() {
-	id := cfg.ID
-	client, err := Login()
-	if err != nil {
-		log.Fatalf("%s: %v", err.Message, err.Error)
-	}
-	downloadURL, FName, client, err := GetUrlDownload(id, client)
-	if err != nil {
-		log.Fatalf("%s: %v", err.Message, err.Error)
-	}
-
-	err = GetAudioFile(downloadURL, id, FName, client)
-	if err != nil {
-		log.Fatalf("%s and %v", err.Message, err.Error)
-	}
+   id := cfg.ID
+   client, err := Login()
+   if err != nil {
+      log.Fatalf("%s: %v", err.Message, err.Error)
+   }
+   downloadURL, FName, client, err := GetUrlDownload(id, client)
+   if err != nil {
+      log.Fatalf("%s: %v", err.Message, err.Error)
+   }
+   err = GetAudioFile(downloadURL, id, FName, client)
+   if err != nil {
+      log.Fatalf("%s and %v", err.Message, err.Error)
+   }
 }
 
 // Login will login the user with the provided credentials
 func Login() (*http.Client, *OnError) {
-	CookieJar, _ := cookiejar.New(nil)
-	client := &http.Client{
-		Jar: CookieJar,
-	}
-	Deez := &DeezStruct{}
-	req, err := newRequest(APIUrl, "POST", nil)
-	args := []string{"null", "deezer.getUserData"}
-	req = addQs(req, args...)
-	resp, err := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal(body, &Deez)
-	if err != nil {
-		return nil, &OnError{err, "Error during getCheckFormLogin Unmarshalling"}
-	}
-	CookieURL, _ := url.Parse(Domain)
-	resp.Body.Close()
-	form := url.Values{}
-	form.Add("type", "login")
-	form.Add("checkFormLogin", Deez.Results.CheckFormLogin)
-	req, err = newRequest(LoginURL, "POST", form.Encode())
-	if err != nil {
-		return nil, &OnError{err, "Error during Login Request"}
-	}
-
-	req.Header.Set("Content-type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
-	resp, err = client.Do(req)
-	if err != nil {
-		return nil, &OnError{err, "Error during Login response"}
-	}
-
-	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, &OnError{err, "Error During Login response Read Body"}
-	}
-	if resp.StatusCode == 200 {
-		addCookies(client, CookieURL)
-		return client, nil
-	}
-	return nil, &OnError{err,
-		"Can't Login, resp status code is" + string(resp.StatusCode)}
+   CookieJar, _ := cookiejar.New(nil)
+   client := &http.Client{Jar: CookieJar}
+   Deez := &DeezStruct{}
+   req, err := newRequest(APIUrl, "POST", nil)
+   args := []string{"null", "deezer.getUserData"}
+   req = addQs(req, args...)
+   resp, err := client.Do(req)
+   body, _ := ioutil.ReadAll(resp.Body)
+   err = json.Unmarshal(body, &Deez)
+   if err != nil {
+      return nil, &OnError{err, "Error during getCheckFormLogin Unmarshalling"}
+   }
+   CookieURL, _ := url.Parse(Domain)
+   resp.Body.Close()
+   form := url.Values{}
+   form.Add("type", "login")
+   form.Add("checkFormLogin", Deez.Results.CheckFormLogin)
+   req, err = newRequest(LoginURL, "POST", form.Encode())
+   if err != nil {
+      return nil, &OnError{err, "Error during Login Request"}
+   }
+   req.Header.Set("Content-type", "application/x-www-form-urlencoded")
+   req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+   resp, err = client.Do(req)
+   if err != nil {
+      return nil, &OnError{err, "Error during Login response"}
+   }
+   defer resp.Body.Close()
+   body, err = ioutil.ReadAll(resp.Body)
+   if err != nil {
+      return nil, &OnError{err, "Error During Login response Read Body"}
+   }
+   if resp.StatusCode == 200 {
+      addCookies(client, CookieURL)
+      return client, nil
+   }
+   return nil, &OnError{
+      err, "Can't Login, resp status code is" + string(resp.StatusCode),
+   }
 }
 
 func addCookies(client *http.Client, CookieURL *url.URL) {
