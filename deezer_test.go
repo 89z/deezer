7ec9e7a451cc48637ab3eaa413d643dbada2dbba
Steven Penny <srpen6@gmail.com>
2021-03-05 22:50:41 -0600

    new file:   main.go

--- deezer_test.go
+++ deezer_test.go
@@ -11,14 +11,6 @@ const token = "0e21c80ef0b963e68cf5d0a951fc918def86c2188a44b33ab353088f15d7b4" +
 "087ed699e6dcd6293514f49439a7d2a7c86bdbcb6e0efae1acd029ec4f267a07b541bfe13872" +
 "c5e5715db846bc784701c3794c328411b5cca332d695b37c1946c1"
 
-func fileSize(name string) (int64, error) {
-   info, e := os.Stat(name)
-   if e != nil {
-      return 0, e
-   }
-   return info.Size(), nil
-}
-
 func TestMain(t *testing.T) {
    c := exec.Command("deezer.exe", "-id", "75498418", "-usertoken", token)
    c.Stderr, c.Stdout = os.Stderr, os.Stdout
