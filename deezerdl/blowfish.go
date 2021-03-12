07e35a84751ee67ad8244648550690946c7fcc82
Steven Penny <srpen6@gmail.com>
2021-03-12 14:05:28 -0600

    deleted:    deezerdl/utils.go

--- deezerdl/blowfish.go
+++ deezerdl/blowfish.go
@@ -1,13 +1,15 @@
 package deezer
 
 import (
-	"fmt"
-	"io"
-	"os"
-
-	"crypto/cipher"
-
-	"golang.org/x/crypto/blowfish"
+   "crypto/aes"
+   "crypto/cipher"
+   "crypto/md5"
+   "fmt"
+   "golang.org/x/crypto/blowfish"
+   "io"
+   "os"
+   "strconv"
+   "strings"
 )
 
 const blowfishKey = "g4el58wc0zvf9na1"
@@ -91,4 +93,56 @@ func DecryptSongFile(key []byte, inputPath, outputPath string) error {
 	}
 
 	return nil
+}
+
+
+
+var combineChar = []byte("\xa4")
+var deezerKey = []byte("jo6aey6haid2Teih")
+
+func MD5Hash(data []byte) string {
+	hash := md5.Sum(data)
+	return fmt.Sprintf("%x", hash)
+}
+
+// MakeURLPath generates the path of the download URL
+func MakeURLPath(track *Track, format Format) (string, error) {
+	// generate MD5 data
+	chars := []string{
+		track.MD5,
+		strconv.Itoa(int(format)),
+		strconv.Itoa(track.ID),
+		strconv.Itoa(track.MediaVersion),
+	}
+	md5Data := strings.Join(chars, string(combineChar))
+	hash := []byte(MD5Hash([]byte(md5Data)))
+
+	// generate and return hex of encrypted data
+	encData := append(hash, combineChar...)
+	encData = append(encData, md5Data...)
+	encData = append(encData, combineChar...)
+	ecb, err := ECB(deezerKey, encData)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", ecb), nil
+}
+
+func ECB(key, data []byte) ([]byte, error) {
+	cipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	size := cipher.BlockSize()
+	for len(data)%size != 0 {
+		data = append(data, '\x00')
+	}
+
+	encrypted := make([]byte, len(data))
+	for bs, be := 0, size; bs < len(data); bs, be = bs+size, be+size {
+		cipher.Encrypt(encrypted[bs:be], data[bs:be])
+	}
+
+	return encrypted, nil
 }
