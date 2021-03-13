06ebba7bbfe4ece7c77ea1cd72b2e05484ea36a9
Steven Penny <srpen6@gmail.com>
2021-03-12 18:07:35 -0600

    +   cookieJar, err := cookiejar.New(nil)

--- deezerdl/deezer.go
+++ deezerdl/deezer.go
@@ -48,42 +48,13 @@ type API struct {
 }
 
 func NewAPI() (*API, error) {
-	cookieJar, err := cookiejar.New(nil)
-	if err != nil {
-		return nil, err
-	}
-	client := http.Client{
-		Jar: cookieJar,
-	}
-	api := API{
-		client:    &client,
-	}
-	return &api, nil
-}
-
-func (api *API) ApiRequest(method string, body io.Reader) (*http.Response, error) {
-   u := apiUrl
-   q := url.Values{
-      "api_version": {"1.0"}, "input":       {"3"}, "method":      {method},
-   }
-   if method == getTokenMethod {
-      q.Set("api_token", "null")
-   } else {
-      q.Set("api_token", api.APIToken)
-   }
-   u.RawQuery = q.Encode()
-   req, err := http.NewRequest(
-      http.MethodPost, u.String(), body,
-   )
+   cookieJar, err := cookiejar.New(nil)
    if err != nil {
       return nil, err
    }
-   req.Header.Add("User-Agent", "PostmanRuntime/7.21.0")
-   resp, err := api.client.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   return resp, nil
+   return &API{
+      client: &http.Client{Jar: cookieJar},
+   }, nil
 }
 
 func (api *API) MobileApiRequest(method string, body io.Reader) (*http.Response, error) {
@@ -116,6 +87,32 @@ func (api *API) MobileApiRequest(method string, body io.Reader) (*http.Response,
    }
    return resp, nil
 }
+
+func (api *API) ApiRequest(method string, body io.Reader) (*http.Response, error) {
+   u := apiUrl
+   q := url.Values{
+      "api_version": {"1.0"}, "input":       {"3"}, "method":      {method},
+   }
+   if method == getTokenMethod {
+      q.Set("api_token", "null")
+   } else {
+      q.Set("api_token", api.APIToken)
+   }
+   u.RawQuery = q.Encode()
+   req, err := http.NewRequest(
+      http.MethodPost, u.String(), body,
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Add("User-Agent", "PostmanRuntime/7.21.0")
+   resp, err := api.client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   return resp, nil
+}
+
 
 func (api *API) CookieLogin(arl string) error {
 	// add the cookie to the jar
@@ -460,20 +457,18 @@ const (
 	downloadPathFormat = "/mobile/1/%s"
 )
 
-type Track struct {
-	ID           int     `json:"SNG_ID,string"`
-	Title        string  `json:"SNG_TITLE"`
-	TrackNumber  int     `json:"TRACK_NUMBER,string"`
-	Gain         float32 `json:"GAIN,string"`
-	MD5          string  `json:"MD5_ORIGIN"`
-	MediaVersion int     `json:"MEDIA_VERSION,string"`
-	api          *API
-}
-
 var NoMD5Error = errors.New("no MD5 hash -- try authenticating")
 
-// GetDownloadURL gets the download url (as a *url.URL) for a given
-// format
+type Track struct {
+   ID           int     `json:"SNG_ID,string"`
+   Title        string  `json:"SNG_TITLE"`
+   TrackNumber  int     `json:"TRACK_NUMBER,string"`
+   Gain         float32 `json:"GAIN,string"`
+   MD5          string  `json:"MD5_ORIGIN"`
+   MediaVersion int     `json:"MEDIA_VERSION,string"`
+   api          *API
+}
+
 func (track *Track) GetDownloadURL(format Format) (*url.URL, error) {
 	if len(track.MD5) == 0 {
 		if err := track.GetMD5(); err != nil {
@@ -492,35 +487,26 @@ func (track *Track) GetDownloadURL(format Format) (*url.URL, error) {
 	return &u, nil
 }
 
-// GetMD5 uses an alternative API to get the MD5 of the track
 func (track *Track) GetMD5() error {
-	resp, err := track.api.MobileApiRequest(getSongMobileMethod,
-		strings.NewReader(fmt.Sprintf(`{"SNG_ID":%d}`, track.ID)))
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	var data struct {
-		Results json.RawMessage `json:"results"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil { // uses the body directly
-		return err
-	}
-	// and then get the MD5
-	var results struct {
-		MD5 string `json:"MD5_ORIGIN"`
-	}
-	if err := json.Unmarshal(data.Results, &results); err != nil {
-		return err
-	}
-
-	if results.MD5 == "" {
-		return NoMD5Error
-	}
-	track.MD5 = results.MD5
-
-	return nil
-
+   resp, err := track.api.MobileApiRequest(getSongMobileMethod,
+   strings.NewReader(fmt.Sprintf(`{"SNG_ID":%d}`, track.ID)))
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   var data struct{Results json.RawMessage `json:"results"`}
+   if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+      return err
+   }
+   var results struct {MD5 string `json:"MD5_ORIGIN"`}
+   if err := json.Unmarshal(data.Results, &results); err != nil {
+      return err
+   }
+   if results.MD5 == "" {
+      return NoMD5Error
+   }
+   track.MD5 = results.MD5
+   return nil
 }
 
 // GetSongData gets a track
